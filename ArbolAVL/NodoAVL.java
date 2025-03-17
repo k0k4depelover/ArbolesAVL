e2964d559eb303e1906d7d0eff6cb4fafe3b233e
marioo-t <mariotaracena001@gmail.com>
2025-03-16 22:01:04 -0600

    Update NodoAVL.java
    Aquí esta todo lo que me encargaron para el proyecto

--- ArbolAVL/NodoAVL.java
+++ ArbolAVL/NodoAVL.java
@@ -1,14 +1,53 @@
 package ArbolAVL;
 
-import java.util.ArrayList;
 public class NodoAVL {
-	int dato, FE;
-	NodoAVL izq, der;
-	
-	public NodoAVL(int dato) {
-		this.dato=dato;
-		this.FE=0;
-		this.izq= this.der =null;
-	}
-	
+    int dato, FE;
+    NodoAVL izq, der;
+    
+    // Constructor
+    public NodoAVL(int dato) {
+        this.dato = dato;
+        this.FE = 0;
+        this.izq = this.der = null;
+    }
+
+    // Obtener el valor del nodo
+    public int getValor() {
+        return this.dato;
+    }
+
+    // Modificar el valor del nodo
+    public void setValor(int valor) {
+        this.dato = valor;
+    }
+
+    // Obtener el factor de equilibrio
+    public int getFactorEquilibrio() {
+        return this.FE;
+    }
+
+    // Modificar el factor de equilibrio
+    public void setFactorEquilibrio(int fe) {
+        this.FE = fe;
+    }
+
+    // Obtener el hijo izquierdo
+    public NodoAVL getHijoIzquierdo() {
+        return this.izq;
+    }
+
+    // Modificar el hijo izquierdo
+    public void setHijoIzquierdo(NodoAVL izq) {
+        this.izq = izq;
+    }
+
+    // Obtener el hijo derecho
+    public NodoAVL getHijoDerecho() {
+        return this.der;
+    }
+
+    // Modificar el hijo derecho
+    public void setHijoDerecho(NodoAVL der) {
+        this.der = der;
+    }
 }
